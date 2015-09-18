4312432fbc7964d45bbfb6d607a97481597d877f
buchslava <vyacheslav.chub@gmail.com>
2015-09-18 10:40:01 +0300

    optim. event init

--- components/handsontable/handsontable.ts
+++ components/handsontable/handsontable.ts
@@ -43,64 +43,6 @@ export class HotTable {
   private inst:any;
   private view:any;
 
-  // events
-  private afterCellMetaReset:EventEmitter = new EventEmitter();
-  private afterChange:EventEmitter = new EventEmitter();
-  private afterCreateCol:EventEmitter = new EventEmitter();
-  private afterCreateRow:EventEmitter = new EventEmitter();
-  private afterDeselect:EventEmitter = new EventEmitter();
-  private afterDestroy:EventEmitter = new EventEmitter();
-  private afterDocumentKeyDown:EventEmitter = new EventEmitter();
-  private afterGetCellMeta:EventEmitter = new EventEmitter();
-  private afterGetColHeader:EventEmitter = new EventEmitter();
-  private afterGetRowHeader:EventEmitter = new EventEmitter();
-  private afterInit:EventEmitter = new EventEmitter();
-  private afterIsMultipleSelectionCheck:EventEmitter = new EventEmitter();
-  private afterLoadData:EventEmitter = new EventEmitter();
-  private afterMomentumScroll:EventEmitter = new EventEmitter();
-  private afterOnCellCornerMouseDown:EventEmitter = new EventEmitter();
-  private afterOnCellMouseDown:EventEmitter = new EventEmitter();
-  private afterOnCellMouseOver:EventEmitter = new EventEmitter();
-  private afterRemoveCol:EventEmitter = new EventEmitter();
-  private afterRemoveRow:EventEmitter = new EventEmitter();
-  private afterRender:EventEmitter = new EventEmitter();
-  private afterRenderer:EventEmitter = new EventEmitter();
-  private afterScrollHorizontally:EventEmitter = new EventEmitter();
-  private afterScrollVertically:EventEmitter = new EventEmitter();
-  private afterSelection:EventEmitter = new EventEmitter();
-  private afterSelectionByProp:EventEmitter = new EventEmitter();
-  private afterSelectionEnd:EventEmitter = new EventEmitter();
-  private afterSelectionEndByProp:EventEmitter = new EventEmitter();
-  private afterSetCellMeta:EventEmitter = new EventEmitter();
-  private afterUpdateSettings:EventEmitter = new EventEmitter();
-  private afterValidate:EventEmitter = new EventEmitter();
-  private beforeAutofill:EventEmitter = new EventEmitter();
-  private beforeCellAlignment:EventEmitter = new EventEmitter();
-  private beforeChange:EventEmitter = new EventEmitter();
-  private beforeChangeRender:EventEmitter = new EventEmitter();
-  private beforeDrawBorders:EventEmitter = new EventEmitter();
-  private beforeGetCellMeta:EventEmitter = new EventEmitter();
-  private beforeInit:EventEmitter = new EventEmitter();
-  private beforeInitWalkontable:EventEmitter = new EventEmitter();
-  private beforeKeyDown:EventEmitter = new EventEmitter();
-  private beforeOnCellMouseDown:EventEmitter = new EventEmitter();
-  private beforeRemoveCol:EventEmitter = new EventEmitter();
-  private beforeRemoveRow:EventEmitter = new EventEmitter();
-  private beforeRender:EventEmitter = new EventEmitter();
-  private beforeSetRangeEnd:EventEmitter = new EventEmitter();
-  private beforeTouchScroll:EventEmitter = new EventEmitter();
-  private beforeValidate:EventEmitter = new EventEmitter();
-  private construct:EventEmitter = new EventEmitter();
-  private init:EventEmitter = new EventEmitter();
-  private modifyCol:EventEmitter = new EventEmitter();
-  private modifyColWidth:EventEmitter = new EventEmitter();
-  private modifyRow:EventEmitter = new EventEmitter();
-  private modifyRowHeight:EventEmitter = new EventEmitter();
-  private persistentStateLoad:EventEmitter = new EventEmitter();
-  private persistentStateReset:EventEmitter = new EventEmitter();
-  private persistentStateSave:EventEmitter = new EventEmitter();
-  /// events
-
   private data:Array<any> = [];
   private colHeaders:Array<string> = [];
   private columns:Array<any> = [];
@@ -110,6 +52,10 @@ export class HotTable {
   private height:number;
 
   constructor(private element:ElementRef) {
+    // fill events dynamically
+    eventNames.forEach(eventName => {
+      this[eventName] = new EventEmitter();
+    });
   }
 
   parseAutoComplete(column, dataSet) {
