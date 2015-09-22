88d9ad113f7ac63f5517ff694c4a14a57e65fb35
buchslava <vyacheslav.chub@gmail.com>
2015-09-22 11:36:30 +0300

    fixes and optimizations for demo

--- demo/components/handsontable-section.ts
+++ demo/components/handsontable-section.ts
@@ -15,229 +15,103 @@ let name = 'Handsontable';
 let src = 'https://github.com/valor-software/ng2-handsontable/blob/master/components/handsontable/handsontable.ts';
 // webpack html imports
 let doc = require('../../components/handsontable/readme.md');
-let titleDoc = require('../../components/handsontable/title.md');
 
-let tsBasic = require('!!prismjs?lang=typescript!./handsontable/basic-demo.ts');
-let htmlBasic = require('!!prismjs?lang=markup!./handsontable/basic-demo.html');
-let tsSheet = require('!!prismjs?lang=typescript!./handsontable/sheet-demo.ts');
-let htmlSheet = require('!!prismjs?lang=markup!./handsontable/sheet-demo.html');
-let tsPersonal = require('!!prismjs?lang=typescript!./handsontable/personal-demo.ts');
-let htmlPersonal = require('!!prismjs?lang=markup!./handsontable/personal-demo.html');
-let tsFinance = require('!!prismjs?lang=typescript!./handsontable/finance-demo.ts');
-let htmlFinance = require('!!prismjs?lang=markup!./handsontable/finance-demo.html');
-let tsScience = require('!!prismjs?lang=typescript!./handsontable/science-demo.ts');
-let htmlScience = require('!!prismjs?lang=markup!./handsontable/science-demo.html');
-let tsSport = require('!!prismjs?lang=typescript!./handsontable/sport-demo.ts');
-let htmlSport = require('!!prismjs?lang=markup!./handsontable/sport-demo.html');
-let tsAdvanced = require('!!prismjs?lang=typescript!./handsontable/advanced-demo.ts');
-let htmlAdvanced = require('!!prismjs?lang=markup!./handsontable/advanced-demo.html');
+let tabDesc:Array<any> = [
+  {
+    heading: 'Basic',
+    ts: require('!!prismjs?lang=typescript!./handsontable/basic-demo.ts'),
+    html: require('!!prismjs?lang=markup!./handsontable/basic-demo.html')
+  },
+  {
+    heading: 'Advanced',
+    ts: require('!!prismjs?lang=typescript!./handsontable/advanced-demo.ts'),
+    html: require('!!prismjs?lang=markup!./handsontable/advanced-demo.html'),
+    source: 'https://www.quandl.com/c/demography/total-population-by-country'
+
+  },
+  {
+    heading: 'Sheet',
+    ts: require('!!prismjs?lang=typescript!./handsontable/sheet-demo.ts'),
+    html: require('!!prismjs?lang=markup!./handsontable/sheet-demo.html')
+
+  }
+  ,
+  {
+    heading: 'Personal',
+    ts: require('!!prismjs?lang=typescript!./handsontable/personal-demo.ts'),
+    html: require('!!prismjs?lang=markup!./handsontable/personal-demo.html')
+
+  },
+  {
+    heading: 'Finance',
+    ts: require('!!prismjs?lang=typescript!./handsontable/finance-demo.ts'),
+    html: require('!!prismjs?lang=markup!./handsontable/finance-demo.html'),
+    source: 'https://www.quandl.com/c/markets/bitcoin-data'
+  },
+  {
+    heading: 'Science',
+    ts: require('!!prismjs?lang=typescript!./handsontable/science-demo.ts'),
+    html: require('!!prismjs?lang=markup!./handsontable/science-demo.html'),
+    source: 'https://plot.ly/~JStevens/0/an-age-distribution-for-scientific-genius'
+  },
+  {
+    heading: 'Sport',
+    ts: require('!!prismjs?lang=typescript!./handsontable/sport-demo.ts'),
+    html: require('!!prismjs?lang=markup!./handsontable/sport-demo.html'),
+    source: 'http://www.forbes.com/nba-valuations/list'
+  }
+];
 
 let dataLink = `<p>All data completes that used in these examples are available in
   <a href="https://github.com/valor-software/ng2-handsontable/blob/master/demo/components/handsontable/data.ts">next file</a>.</p>`;
+
+let tabsContent:string = ``;
+tabDesc.forEach(desc => {
+  let source = '';
+  if (desc.source) {
+    source = `Source of data: <a href="${desc.source}" target="_blank">${desc.source}</a><br>`;
+  }
+
+  tabsContent += `
+          <tab heading="${desc.heading}" (select)="select($event)">
+          <div class="card card-block panel panel-default panel-body">
+
+            <${desc.heading.toLowerCase()}-demo *ng-if="currentHeading === '${desc.heading}'"></${desc.heading.toLowerCase()}-demo>
+
+            <br>
+
+            <div class="row" style="margin: 0px;">
+              <tabset>
+                <tab heading="Markup">
+                  <div class="card card-block panel panel-default panel-body">
+                    <pre class="language-html"><code class="language-html" ng-non-bindable>${desc.html}</code></pre>
+                  </div>
+                </tab>
+                <tab heading="TypeScript">
+                  <div class="card card-block panel panel-default panel-body">
+                    <pre class="language-typescript"><code class="language-typescript" ng-non-bindable>${desc.ts}</code></pre>
+                  </div>
+                </tab>
+              </tabset>
+              ${source}
+              ${dataLink}
+            </div>
+          </div>
+        </tab>
+  `;
+});
 
 @Component({
   selector: 'handsontable-section'
 })
 @View({
   template: `
-  <!--<br>-->
   <section id="${name.toLowerCase()}">
-    <!--<div class="row"><h1>${name}<small>(<a href="${src}">src</a>)</small></h1></div>
-
-    <hr>
-
-    <div class="row"><div class="col-md-12">${titleDoc}</div></div>
-
-    <h2>Examples</h2>-->
-
     <div class="row">
       <tabset>
 
-        <tab heading="Basic" (select)="select($event)">
-          <div class="card card-block panel panel-default panel-body">
+        ${tabsContent}
 
-            <basic-demo *ng-if="currentHeading === 'Basic'"></basic-demo>
-
-            <br>
-
-            <div class="row" style="margin: 0px;">
-              <tabset>
-                <tab heading="Markup">
-                  <div class="card card-block panel panel-default panel-body">
-                    <pre class="language-html"><code class="language-html" ng-non-bindable>${htmlBasic}</code></pre>
-                  </div>
-                </tab>
-                <tab heading="TypeScript">
-                  <div class="card card-block panel panel-default panel-body">
-                    <pre class="language-typescript"><code class="language-typescript" ng-non-bindable>${tsBasic}</code></pre>
-                  </div>
-                </tab>
-              </tabset>
-
-              ${dataLink}
-            </div>
-          </div>
-        </tab>
-
-        <tab heading="Advanced" (select)="select($event)">
-          <div class="card card-block panel panel-default panel-body">
-
-            <advanced-demo *ng-if="currentHeading === 'Advanced'"></advanced-demo>
-
-            <br>
-
-            <div class="row" style="margin: 0px;">
-              <tabset>
-                <tab heading="Markup">
-                  <div class="card card-block panel panel-default panel-body">
-                    <pre class="language-html"><code class="language-html" ng-non-bindable>${htmlAdvanced}</code></pre>
-                  </div>
-                </tab>
-                <tab heading="TypeScript">
-                  <div class="card card-block panel panel-default panel-body">
-                    <pre class="language-typescript"><code class="language-typescript" ng-non-bindable>${tsAdvanced}</code></pre>
-                  </div>
-                </tab>
-              </tabset>
-
-              ${dataLink}<br>
-              Source of data: <a href="https://www.quandl.com/c/demography/total-population-by-country" target="_blank">https://www.quandl.com/c/demography/total-population-by-country</a>
-            </div>
-          </div>
-        </tab>
-
-        <tab heading="Sheet" (select)="select($event)">
-          <div class="card card-block panel panel-default panel-body">
-
-            <sheet-demo *ng-if="currentHeading === 'Sheet'"></sheet-demo>
-
-            <br>
-
-            <div class="row" style="margin: 0px;">
-              <tabset>
-                <tab heading="Markup">
-                  <div class="card card-block panel panel-default panel-body">
-                    <pre class="language-html"><code class="language-html" ng-non-bindable>${htmlSheet}</code></pre>
-                  </div>
-                </tab>
-                <tab heading="TypeScript">
-                  <div class="card card-block panel panel-default panel-body">
-                    <pre class="language-typescript"><code class="language-typescript" ng-non-bindable>${tsSheet}</code></pre>
-                  </div>
-                </tab>
-              </tabset>
-
-              ${dataLink}
-            </div>
-          </div>
-        </tab>
-
-        <tab heading="Personal" (select)="select($event)">
-          <div class="card card-block panel panel-default panel-body">
-
-            <personal-demo *ng-if="currentHeading === 'Personal'"></personal-demo>
-
-            <br>
-
-            <div class="row" style="margin: 0px;">
-              <tabset>
-                <tab heading="Markup">
-                  <div class="card card-block panel panel-default panel-body">
-                    <pre class="language-html"><code class="language-html" ng-non-bindable>${htmlPersonal}</code></pre>
-                  </div>
-                </tab>
-                <tab heading="TypeScript">
-                  <div class="card card-block panel panel-default panel-body">
-                    <pre class="language-typescript"><code class="language-typescript" ng-non-bindable>${tsPersonal}</code></pre>
-                  </div>
-                </tab>
-              </tabset>
-
-              ${dataLink}
-            </div>
-          </div>
-        </tab>
-
-        <tab heading="Finance" (select)="select($event)">
-          <div class="card card-block panel panel-default panel-body">
-
-            <finance-demo *ng-if="currentHeading === 'Finance'"></finance-demo>
-
-            <br>
-
-            <div class="row" style="margin: 0px;">
-              <tabset>
-                <tab heading="Markup">
-                  <div class="card card-block panel panel-default panel-body">
-                    <pre class="language-html"><code class="language-html" ng-non-bindable>${htmlFinance}</code></pre>
-                  </div>
-                </tab>
-                <tab heading="TypeScript">
-                  <div class="card card-block panel panel-default panel-body">
-                    <pre class="language-typescript"><code class="language-typescript" ng-non-bindable>${tsFinance}</code></pre>
-                  </div>
-                </tab>
-              </tabset>
-
-              ${dataLink}<br>
-              Source of data: <a href="https://www.quandl.com/c/markets/bitcoin-data" target="_blank">https://www.quandl.com/c/markets/bitcoin-data</a>
-            </div>
-          </div>
-        </tab>
-
-        <tab heading="Science" (select)="select($event)">
-          <div class="card card-block panel panel-default panel-body">
-
-            <science-demo *ng-if="currentHeading === 'Science'"></science-demo>
-
-            <br>
-
-            <div class="row" style="margin: 0px;">
-              <tabset>
-                <tab heading="Markup">
-                  <div class="card card-block panel panel-default panel-body">
-                    <pre class="language-html"><code class="language-html" ng-non-bindable>${htmlScience}</code></pre>
-                  </div>
-                </tab>
-                <tab heading="TypeScript">
-                  <div class="card card-block panel panel-default panel-body">
-                    <pre class="language-typescript"><code class="language-typescript" ng-non-bindable>${tsScience}</code></pre>
-                  </div>
-                </tab>
-              </tabset>
-
-              ${dataLink}<br>
-              Source of data: <a href="https://plot.ly/~JStevens/0/an-age-distribution-for-scientific-genius" target="_blank">https://plot.ly/~JStevens/0/an-age-distribution-for-scientific-genius</a>
-            </div>
-          </div>
-        </tab>
-
-        <tab heading="Sport" (select)="select($event)">
-          <div class="card card-block panel panel-default panel-body">
-
-            <sport-demo *ng-if="currentHeading === 'Sport'"></sport-demo>
-
-            <br>
-
-            <div class="row" style="margin: 0px;">
-              <tabset>
-                <tab heading="Markup">
-                  <div class="card card-block panel panel-default panel-body">
-                    <pre class="language-html"><code class="language-html" ng-non-bindable>${htmlSport}</code></pre>
-                  </div>
-                </tab>
-                <tab heading="TypeScript">
-                  <div class="card card-block panel panel-default panel-body">
-                    <pre class="language-typescript"><code class="language-typescript" ng-non-bindable>${tsSport}</code></pre>
-                  </div>
-                </tab>
-              </tabset>
-
-              ${dataLink}<br>
-              Source of data: <a href="http://www.forbes.com/nba-valuations/list" target="_blank">http://www.forbes.com/nba-valuations/list</a>
-            </div>
-          </div>
-        </tab>
       </tabset>
     </div>
 
@@ -254,6 +128,8 @@ export class HandsontableSection {
   private currentHeading:string = 'Basic';
 
   private select(e) {
-    this.currentHeading = e.heading;
+    if (e.heading) {
+      this.currentHeading = e.heading;
+    }
   }
 }
