e9043c4043ad4a7583b43cf3f4fcb1036802ec86
Rene Hamburger <rene.hamburger@leanix.net>
2016-11-08 16:18:51 +0100

    Introduces [pagedData] Observable Input & updates options on change

--- components/handsontable/handsontable.ts
+++ components/handsontable/handsontable.ts
@@ -1,7 +1,9 @@
-import {OnInit, OnDestroy, Directive, EventEmitter, ElementRef, Input} from '@angular/core';
+import {OnInit, OnDestroy, OnChanges, SimpleChanges, Directive, EventEmitter,
+  ElementRef, Input} from '@angular/core';
+import {Observable, Subscription} from 'rxjs';
 import * as Handsontable from 'handsontable/dist/handsontable.full.js';
 
-let eventNames:Array<string> = ['afterCellMetaReset', 'afterChange',
+let eventNames: Array<string> = ['afterCellMetaReset', 'afterChange',
   'afterCreateCol', 'afterCreateRow', 'afterDeselect',
   'afterDestroy', 'afterDocumentKeyDown', 'afterGetCellMeta', 'afterGetColHeader', 'afterGetRowHeader',
   'afterInit', 'afterIsMultipleSelectionCheck', 'afterLoadData',
@@ -20,65 +22,127 @@ let eventNames:Array<string> = ['afterCellMetaReset', 'afterChange',
   selector: 'hot-table',
   outputs: eventNames
 })
-export class HotTable implements OnInit, OnDestroy {
-  private inst:any;
-  private view:any;
+export class HotTable implements OnInit, OnDestroy, OnChanges {
+  private inst: any;
+  private view: any;
+  private pagedDataSubscription: Subscription;
 
-  @Input() private data:Array<any> = [];
-  @Input('col-headers') private colHeaders:Array<string>;
-  @Input() private columns:Array<any>;
-  @Input('col-widths') private colWidths:Array<number>;
-  @Input() private options:any;
+  @Input() private data: Array<any> = [];
+  @Input('pagedData') private pagedData$: Observable<Array<any>>;
+  @Input('col-headers') private colHeaders: Array<string>;
+  @Input() private columns: Array<any>;
+  @Input('col-widths') private colWidths: Array<number>;
+  @Input() private options: any;
 
-  constructor(private element:ElementRef) {
+  constructor(private element: ElementRef) {
     // fill events dynamically
     eventNames.forEach(eventName => {
       this[eventName] = new EventEmitter();
     });
   }
 
-  parseAutoComplete(column, dataSet) {
+  private parseAutoComplete(options: any) {
     let inst = this.inst;
+    const columns = this.columns || options.columns;
+    const dataSet = options.data;
 
-    if (typeof column.source === 'string') {
-      let relatedField:string = column.source;
-      column.source = function (query, process) {
-        let row:number = inst.getSelected()[0];
-        let data:any = dataSet[row];
+    if (columns) {
+      columns.forEach(column => {
+        if (typeof column.source === 'string') {
+          let relatedField: string = column.source;
+          column.source = function (query, process) {
+            let row: number = inst.getSelected()[0];
+            let data: any = dataSet[row];
 
-        if (!data) {
-          return;
+            if (!data) {
+              return;
+            }
+
+            let fieldParts: Array<string> = relatedField.split('.');
+            let o:any = data;
+            for (let i = 0; i < fieldParts.length; i++) {
+              o = o[fieldParts[i]];
+            }
+
+            process(o.map(item => {
+              return !column.optionField ? item : item[column.optionField];
+            }));
+          };
         }
-
-        let fieldParts:Array<string> = relatedField.split('.');
-        let o:any = data;
-        for (let i = 0; i < fieldParts.length; i++) {
-          o = o[fieldParts[i]];
-        }
-
-        process(o.map(item => {
-          return !column.optionField ? item : item[column.optionField];
-        }));
-      };
+      });
     }
   }
 
   ngOnInit() {
+    this.checkInputs();
+
     this.view = document.createElement('div');
     this.view.class = 'handsontable-container';
     this.element.nativeElement.appendChild(this.view);
 
-    let htOptions:any = {
-      data: this.data
+    const options = this.getCurrentOptions();
+
+    this.inst = new Handsontable(this.view, options);
+
+    this.parseAutoComplete(options);
+
+    if (this.pagedData$) {
+      this.data = [];
+      this.pagedDataSubscription = this.pagedData$.subscribe((newPagedData) => {
+        Array.prototype.push.apply(this.data, newPagedData);
+        this.inst.loadData(this.data);
+        this.parseAutoComplete(options);
+        this.inst.updateSettings(options);
+      });
+    }
+  }
+
+  ngOnDestroy() {
+    if (this.view) {
+      this.view.remove();
+    }
+    if (this.pagedDataSubscription) {
+      this.pagedDataSubscription.unsubscribe();
+    }
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    // tslint:disable-next-line:no-string-literal
+    if (changes['data'] && !changes['data'].isFirstChange()) {
+      this.inst.loadData(this.data);
+    }
+
+    if ('options' in changes && this.inst) {
+      this.inst.updateSettings(this.getCurrentOptions());
+    }
+  }
+
+  private checkInputs(): boolean {
+    const dataCount = Number(!!this.pagedData$) + Number(!!this.data) +
+      Number(!!(this.options && this.options.data));
+    if (dataCount > 1) {
+      console.error('[pagedData], [data] and [options.data] are all mutually' +
+       ' exclusive');
+      return false;
+    } else if (dataCount === 0) {
+      console.error('One of [pagedData], [data] and [options.data] needs' +
+        ' to be provided');
+      return false;
+    }
+  }
+
+  private getCurrentOptions(): any {
+    let htOptions: any = {
+      data: this.data || null
     };
 
     eventNames.forEach(eventName => {
       htOptions[eventName] = data => {
-        this[eventName].next(data);
+        this[eventName].emit(data);
       };
     });
 
-    let additionalFields:Array<string> = ['colHeaders', 'colWidths', 'columns'];
+    let additionalFields: Array<string> = ['colHeaders', 'colWidths', 'columns'];
     additionalFields.forEach(field => {
       if (this[field]) {
         Object.assign(htOptions, {
@@ -91,20 +155,8 @@ export class HotTable implements OnInit, OnDestroy {
       Object.assign(htOptions, this.options);
     }
 
-    this.inst = Handsontable(this.view, htOptions);
-
-    if (this.columns && this.columns.length) {
-      this.columns.forEach(column => {
-        this.parseAutoComplete(column, this.data);
-      });
-    }
-  }
-
-  ngOnDestroy() {
-    if (this.view) {
-      this.view.remove();
-    }
+    return htOptions;
   }
 }
 
-export const handsontable:Array<any> = [HotTable];
+export const handsontable: Array<any> = [HotTable];
